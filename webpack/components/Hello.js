917fdc112b141435ee66718123ca9ad82f29e871
Allison Zadrozny <allison@zadrozny.com>
2016-05-02 10:50:51 -0500

    Add React component and mount it

---
+++ webpack/components/Hello.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+  render() {
+    return (
+      <div>Hey, y’all!!</div>
+    )
+  }
+}
+export default Hello;
--- webpack/entry.js
+++ webpack/entry.js
@@ -1,1 +1,13 @@
-console.log("Hello, amigos!!");
+import React, { Component } from 'react';
+import {render} from 'react-dom';
+import Hello from './components/Hello';
+
+class App extends Component {
+  render() {
+    return (
+      <Hello />
+    )
+  }
+}
+
+render(<App />, document.getElementById('root'));
