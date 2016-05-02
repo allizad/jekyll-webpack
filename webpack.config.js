bc1d117ffaf1bb97550262bd3fedc6b7857013b7
Allison Zadrozny <allison@zadrozny.com>
2016-05-02 10:24:49 -0500

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  // webpack folder's entry js - excluded from jekll's build process.
+  entry: "./webpack/entry.js",
+  output: {
+    // we're going to put the generated file in the assets folder so jekyll will grab it.
+      path: 'src/assets/javascripts/',
+      filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel', // 'babel-loader' is also a legal name to reference
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+};
