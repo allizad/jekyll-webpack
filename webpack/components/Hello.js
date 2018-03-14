bd1de7785e6e33d1297e6089711f5fe5dffdec20
Hoang Pham <hp@hellofresh.com>
2018-03-14 12:16:48 +0100

    update babel, webpack, react

--- webpack/components/Hello.js
+++ webpack/components/Hello.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 
 class Hello extends Component {
-  render() {
-    return (
-      <div>Hey, y’all!! This is a React Component</div>
-    )
-  }
+    render() {
+        return <div>Hey, y’all!! This is a React Component</div>;
+    }
 }
 export default Hello;
